0d6cf6ac1e5148e35b1907c98b2a40854c656bd7
Sergio Cabral <git@sergiocabral.com>
2021-09-03 20:14:07 -0300

    animação da imagem

--- src/templates/rotate-image/rotate-image.tsx
+++ src/templates/rotate-image/rotate-image.tsx
@@ -12,19 +12,24 @@ export type RotateImageProps = {
 export const RotateImage = (props: RotateImageProps) => {
   return (
     <div className="rotate-image">
-      <div className="primary" style={ {
-        width: props.widthAndHeight,
-        height: props.widthAndHeight,
-        backgroundImage: `url(${props.primary})`,
-        animation: `spinner ${props.cycleInSeconds}s linear infinite`,
-        boxShadow: `0px 0px 15px 0px rgba(255,255,255,0.5), 0px 0px 150px 0px ${props.lightColor}, inset 0px 0px 10px 0px black`
+      <div className="container" style={ {
+          width: props.widthAndHeight,
+          height: props.widthAndHeight
       } }>
-        <div className="secondary" style={ { 
+        <div className="primary" style={ {
           width: props.widthAndHeight,
           height: props.widthAndHeight,
-          backgroundImage: `url(${props.secondary})`,
-          animation: `hide ${props.cycleInSeconds}s linear infinite`
+          backgroundImage: `url(${props.primary})`,
+          animation: `spinner ${props.cycleInSeconds}s linear infinite, showing 6s`,
+          boxShadow: `0px 0px 15px 0px rgba(255,255,255,0.5), 0px 0px 150px 0px ${props.lightColor}, inset 0px 0px 10px 0px black`
         } }>
+          <div className="secondary" style={ { 
+            width: props.widthAndHeight,
+            height: props.widthAndHeight,
+            backgroundImage: `url(${props.secondary})`,
+            animation: `hide ${props.cycleInSeconds}s linear infinite`
+          } }>
+          </div>
         </div>
       </div>
     </div>
